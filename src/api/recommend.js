a9271a6bf2ae289e458b10e16959bbd05af1875f
sunshine824 <cx790168710@aliyun.com>
2017-07-04 10:50:01 +0800

    推荐页面

---
+++ src/api/recommend.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Gatsby on 2017/7/4.
+ */
+import jsonp from '../common/js/jsonp'
+import {commonParams, options} from './config'
+
+export function getRecommend() {
+  const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'h5',
+    needNewCode: 1,
+    uin: 0
+  })
+  return jsonp(url, data, options)
+}
