c6494b46d37360ad4ef466bbf1d189d12f07543c
sunshine824 <cx790168710@aliyun.com>
2017-07-05 10:52:38 +0800

    推荐页面

---
+++ src/components/item/item.vue
@@ -0,0 +1,71 @@
+<template>
+  <li class="item_music">
+    <a href="javascript:;" class="list_main">
+      <div class="list_media">
+        <img :src="data.picUrl" class="item_img"/>
+        <span class="icon_play"></span>
+      </div>
+      <div class="media_info">
+        <h3 class="list_tit">{{data.Ftitle}}</h3>
+      </div>
+    </a>
+  </li>
+</template>
+
+<script type="text/ecmascript-6">
+  export default{
+    props: ['data'],
+    data(){
+      return {}
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .item_music
+    float: left
+    width: 50%
+    -webkit-box-sizing: border-box
+    padding-right: 8px
+    margin-bottom: 10px
+    overflow: hidden
+    .list_main
+      display: block
+      .list_media
+        position: relative
+        margin-bottom: 5px
+        &:before
+          content: ""
+          display: block
+          padding-top: 100%
+        .item_img
+          position: absolute
+          top: 0
+          left: 0
+          display: block
+          width: 100%
+          z-index: 1
+        .icon_play
+          height: 24px
+          bottom: 5px
+          right: 5px
+          width: 24px
+          background-position: 0 0
+          background-image: url("../../../static/list_sprite.png")
+          background-repeat: no-repeat
+          background-size: 24px 60px
+          position: absolute
+          display: block
+          z-index:10
+      .media_info
+        text-align: center
+        color: #fff
+        .list_tit
+          height: 26px
+          line-height: 18px
+          white-space: normal
+          word-wrap: break-word
+          font-size 14px
+          overflow: hidden
+          text-overflow:ellipsis
+</style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -2,20 +2,21 @@
   <div class="recommend">
     <div class="recommend-content">
       <div class="slider-wrapper">
-        <slider :data="recommends ? recommends : ''"></slider>
+        <slider :data="slider ? slider : ''"></slider>
       </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul>
-          <li v-for="(item,index) in discList" key="index" class="item">
-            <div class="icon">
-              <img :src="item.imgurl" width="60" height="60"/>
-            </div>
-            <div class="text">
-              <h2 class="name" v-html="item.creator.name"></h2>
-              <p class="desc" v-html="item.dissname"></p>
-            </div>
-          </li>
+      <div class="radio-list">
+        <h1 class="list-title">电台推荐</h1>
+        <ul class="radio_wrapper">
+          <!--<li v-for="(item,index) in discList" key="index" class="item">-->
+          <!--<div class="icon">-->
+          <!--<img :src="item.imgurl" width="60" height="60"/>-->
+          <!--</div>-->
+          <!--<div class="text">-->
+          <!--<h2 class="name" v-html="item.creator.name"></h2>-->
+          <!--<p class="desc" v-html="item.dissname"></p>-->
+          <!--</div>-->
+          <!--</li>-->
+          <item v-for="(item,index) in radioList" key="index" :data="item"></item>
         </ul>
       </div>
     </div>
@@ -26,16 +27,19 @@
   import {getRecommend, getDiscList} from '../../api/recommend'
   import {ERR_OK} from '../../api/config'
   import Slider from '../../base/slider/slider'
+  import Item from '../item/item'
 
   export default {
     data(){
       return {
-        recommends: [],
-        discList:[]
+        slider: [],
+        radioList: [],
+        discList: []
       }
     },
     components: {
-      slider: Slider
+      slider: Slider,
+      item: Item
     },
     created() {
       this._getRecommend()
@@ -45,15 +49,16 @@
       _getRecommend() {
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
-              console.log(res.data)
-            this.recommends = res.data.slider
+            console.log(res.data)
+            this.slider = res.data.slider
+            this.radioList = res.data.radioList
           }
         })
       },
       _getDiscList(){
         getDiscList().then((res) => {
           if (res.code === ERR_OK) {
-            this.discList=res.data.list
+            this.discList = res.data.list
           }
         })
       }
@@ -75,13 +80,18 @@
         width: 100%
         overflow: hidden
         padding-top: 40%
-      .recommend-list
+      .radio-list
+        margin-left: 10px
+        margin-right: 10px
         .list-title
           height: 65px
           line-height: 65px
           text-align: center
           font-size: $font-size-medium
           color: $color-theme
+        .radio_wrapper
+          overflow: hidden
+          margin-right: -8px
         .item
           display: flex
           box-sizing: border-box
