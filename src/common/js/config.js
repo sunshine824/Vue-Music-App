064fd7413b9cb70184379ca1777d940475aac244
sunshine824 <cx790168710@aliyun.com>
2017-07-10 10:33:31 +0800

    歌手详情页

---
+++ src/common/js/config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Gatsby on 2017/7/10.
+ */
+export const playMode={
+  sequence:0, //顺序播放
+  loop:1,  //循环播放
+  random:2 //随机播放
+}
--- src/store/state.js
+++ src/store/state.js
@@ -1,12 +1,16 @@
 /**
  * Created by Gatsby on 2017/7/6.
  */
+import {playMode} from '../common/js/config'
+
 const state = {
   singer: {},
   playing: false,  //控制音乐播放暂停
   fullScreen: false, //全屏
   playList: [], //播放列表
   sequenceList: [],//播放顺序列表
+  mode: playMode.sequence,
+  currentIndex: -1
 }
 
 export default state
