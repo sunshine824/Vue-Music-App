207c35bed73610f8aea71c11acb6293416a18ac0
sunshine824 <cx790168710@aliyun.com>
2017-07-17 16:57:59 +0800

    歌词组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Rank from '@/components/rank/rank.vue'
+import Rank from '@/components/rank/rank'
 import Recommend from '@/components/recommend/recommend'
 import Search from '@/components/search/search'
 import Singer from '@/components/singer/singer'
